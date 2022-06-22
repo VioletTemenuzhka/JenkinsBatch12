e58299db2bd20e6c4caec5c6295b4bcd12f7e0c3
VioletTemenuzhka <violet.chan.fl@gmail.com>
2022-06-21 21:10:55 -0400

    My first jenkins project

---
+++ src/com/syntax/Main.java
@@ -0,0 +1,11 @@
+package com.syntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        for (int i=1; i<6; i++){
+            System.out.println("Hello Jenkins");
+        }
+    }
+}
