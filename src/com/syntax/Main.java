b1b085deb47f2864244549b120e9c2756b6514fd
VioletTemenuzhka <violet.chan.fl@gmail.com>
2022-06-23 16:53:45 -0400

    2nd commit

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -6,6 +6,7 @@ public class Main {
 	// write your code here
         for (int i=1; i<6; i++){
             System.out.println("Hello Jenkins");
+            System.out.println("Hello I set the timer for every 1 min");
         }
     }
 }
